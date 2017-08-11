fa7ed15d0471368d59c2c75bfc7d31560d7bcd90
xingmengmeng <xingmengmeng@gomeholdings.com>
2017-08-11 17:34:49 +0800

    less测试

--- app/components/HomeHeader/index.js
+++ app/components/HomeHeader/index.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 import {Route,Link} from 'react-router-dom';
+
+require('./index.less');
 export default class HomeHeader extends Component{
     render(){
         //设置当前页面选中状态
@@ -11,12 +13,12 @@ export default class HomeHeader extends Component{
             )}/>
         )
         return(
-            <div>
+            <div className='header'>
                 头部导航
-                <ul className='cc'>
-                        <ListItemLink to="/home" label="Home"/>
-                        <ListItemLink to="/login" label="Login"/>
-                    </ul>
+                <ul className='nav'>
+                    <ListItemLink to="/home" label="Home"/>
+                    <ListItemLink to="/login" label="Login"/>
+                </ul>
             </div>
         )
     }
